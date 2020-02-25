182d0b8e7fedb2d7df493362ba908d5679b3b0bc
harshit860 <harshitga@gmail.com>
2020-02-26 00:32:00 +0530

    Initial smart commit

---
+++ project.py
@@ -0,0 +1,28 @@
+import webbrowser
+print('---------------------------')
+print('---------------------------')
+print('Welcome to buddy your friend')
+
+
+first = input('What is your name')
+last = input(first+' '+'last name please')
+gender = input('press m for male and f for female')
+
+if gender == 'm':
+    age = input('what is your age Mr '+first)
+else:
+    age = input('what is your age Ms '+first)
+
+if first[0] == 'h' or first[1] =='a' or first == 'harshit':
+    print('Hello admin')
+
+if int(age) > 25:
+    remark = 'Young'
+
+choice = input(remark+' '+'choice from these options :-')
+
+
+if choice == 'y':
+    webbrowser.open('https://www.youtube.com')
+else:
+    Warning('hey there')
